7c0558186dbad189f89a488a265a3a8a349beebc
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-12 09:06:56 -0300

    ex 22.2 - ex pratico 4

--- src/app.js
+++ src/app.js
@@ -1,60 +1,65 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
 
 const activities = [
-    {
-      id: 1,
-      description: 'Banho no cachorro',
-      status: 'A fazer',
-    },
-    {
-      id: 2,
-      description: 'Cortar a grama',
-      status: 'A fazer',
-    },
-    {
-      id: 3,
-      description: 'Estudar JavaScript',
-      status: 'Feito',
-    },
-  ];
+  {
+    id: 1,
+    description: "Banho no cachorro",
+    status: "A fazer",
+  },
+  {
+    id: 2,
+    description: "Cortar a grama",
+    status: "A fazer",
+  },
+  {
+    id: 3,
+    description: "Estudar JavaScript",
+    status: "Feito",
+  },
+];
 
-app.get('/activities', (req, res) => res.status(200).json({ activities }));
+app.get("/myActivities", (req, res) => res.status(200).json({ activities }));
 
-app.post('/activities', (req, res) => {
-    const newTeam = { ...req.body };
-    activities.push(newTeam);
-  
-    res.status(201).json({ team: newTeam });
-  });
+app.get("/myActivities/:id", (req, res) => {
+  const activity = activities.find((e) => e.id === Number(req.params.id));
+  res.status(200).json(activity);
+});
 
-app.put('/activities/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, initials } = req.body;
-    let updatedTeam;
-  
-    for (let i = 0; i < activities.length; i += 1) {
-      const team = activities[i];
-  
-      if (team.id === Number(id)) {
-        team.name = name;
-        team.initials = initials;
-        updatedTeam = team;
-      }
-    }
-  
-    res.status(200).json({ updatedTeam });
-  });
+// app.post('/activities', (req, res) => {
+//     const newActivity = { ...req.body };
+//     activities.push(newActivity);
 
-  app.delete('/activities/:id', (req, res) => {
-    const { id } = req.params;
-    const arrayPosition = activities.findIndex((team) => team.id === Number(id));
-    activities.splice(arrayPosition, 1);
-  
-    res.status(200).end();
-  });
+//     res.status(201).json({ activity: newActivity });
+//   });
+
+// app.put('/activities/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { description, status } = req.body;
+//     let updatedActivity;
+
+//     for (let i = 0; i < activities.length; i += 1) {
+//       const activity = activities[i];
+
+//       if (activity.id === Number(id)) {
+//         activity.description = description;
+//         activity.status = status;
+//         updatedActivity = activity;
+//       }
+//     }
+
+//     res.status(200).json({ updatedActivity });
+//   });
+
+//   app.delete('/activities/:id', (req, res) => {
+//     const { id } = req.params;
+//     const arrayPosition = activities.findIndex((activity) => activity.id === Number(id));
+//     activities.splice(arrayPosition, 1);
+
+//     res.status(200).end();
+//   });
 
 module.exports = app;
